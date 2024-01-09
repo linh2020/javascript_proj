2d3ed2f214fb91a79e5b74797c8287568319f8aa
Linh Huynh <cdev.linh@gmail.com>
2024-01-08 19:21:31 -0800

    Updated miles_to_kilomerters_converter.js

--- miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
+++ miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
@@ -2,51 +2,16 @@
 //
 //
 
-// Best version
-let images = [1, 2, 3, 4, 5];
-let currentImg = 0;
+let btn = document.getElementById("cd-btn");
+let res = document.getElementById("res");
 
-let myImg = document.getElementById("img");
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+btn.addEventListener("click", () => {
+  let miles = document.getElementById("input-value").value;
 
-// myImg.setAttribute("src", `./images/${images[0]}.jpg`);
-
-prev.addEventListener("click", () => {
-  currentImg--;
-  if (currentImg < 0) currentImg = images.length - 1;
-  myImg.setAttribute("src", `./images/${images[currentImg]}.jpg`);
+  if (isNaN(miles)) {
+    res.innerHTML = "Please enter number";
+  } else {
+    let km = miles * 1.609344;
+    res.innerHTML = `${miles} mile(s)= ${km} Km(s)`;
+  }
 });
-
-next.addEventListener("click", () => {
-  currentImg++;
-  if (currentImg > images.length - 1) currentImg = 0;
-  myImg.setAttribute("src", `./images/${images[currentImg]}.jpg`);
-});
-
-// Another version
-// let images = [1, 2, 3, 4, 5];
-
-// let myImg = document.getElementById("img");
-// let prev = document.getElementById("prev");
-// let next = document.getElementById("next");
-
-// myImg.setAttribute("src", `./images/${images[0]}.jpg`);
-
-// prev.addEventListener("click", () => {
-//   let n = myImg.getAttribute("src").substring(9, 10);
-//   --n;
-//   if (n < 1) {
-//     n = 5;
-//   }
-//   myImg.setAttribute("src", `./images/${images[--n]}.jpg`);
-// });
-
-// next.addEventListener("click", () => {
-//   let n = myImg.getAttribute("src").substring(9, 10);
-//   n++;
-//   if (n > 5) {
-//     n = 1;
-//   }
-//   myImg.setAttribute("src", `./images/${images[--n]}.jpg`);
-// });
