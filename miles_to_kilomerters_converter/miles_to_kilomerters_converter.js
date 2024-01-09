9223f72c20b74aeb6fa4c421f74cf98f285149f0
Linh Huynh <cdev.linh@gmail.com>
2024-01-08 23:24:27 -0800

    Updated miles_to_kilomerters_converter.js

--- miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
+++ miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
@@ -9,9 +9,9 @@ btn.addEventListener("click", () => {
   let miles = document.getElementById("input-value").value;
 
   if (isNaN(miles)) {
-    res.innerHTML = "Please enter number";
+    res.innerHTML = "Please enter number!";
   } else {
     let km = miles * 1.609344;
-    res.innerHTML = `${miles} mile(s)= ${km} Km(s)`;
+    res.innerHTML = `${miles} mile(s) = ${km} Km(s)`;
   }
 });
