8cbba7fac79feb5a84d4cd1cfe74796ebacbac9a
Linh Huynh <cdev.linh@gmail.com>
2024-01-09 11:32:25 -0800

    Updated miles_to_kilomerters_converter.js

--- miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
+++ miles_to_kilomerters_converter/miles_to_kilomerters_converter.js
@@ -1,18 +1,38 @@
-//
-//
-//
+let convertType = "mile";
+let title = document.getElementById("title");
+let desc = document.getElementById("desc");
 
-let form = document.getElementById("convert");
-let res = document.getElementById("res");
+document.addEventListener("keydown", (e) => {
+  let key = e.code;
+  // alert(key);
+  if (key === "KeyK") {
+    convertType = "km";
+    title.innerHTML = "MILES TO KILOMETERS CONVERTER";
+    desc.innerHTML =
+      "Type in a number of miles and click the button to convert the distance to kilometers.";
+  } else if (key === "KeyM") {
+    convertType = "mile";
+    title.innerHTML = "KILOMETERS TO MILES CONVERTER";
+    desc.innerHTML =
+      "Type in a number of kilometers and click the button to convert the distance to miles.";
+  }
+});
 
-form.addEventListener("submit", (e) => {
+document.getElementById("convert").addEventListener("submit", (e) => {
   e.preventDefault();
-  let distance = document.getElementById("distance").value;
 
-  if (isNaN(distance)) {
-    res.innerHTML = "Please enter number!";
+  const distance = parseFloat(document.getElementById("distance").value);
+  const result = document.getElementById("result");
+
+  if (!distance) {
+    result.innerHTML = "Please provide a number!";
   } else {
-    let km = distance * 1.609344;
-    res.innerHTML = `${distance} mile(s) = ${km} Km(s)`;
+    if (convertType === "mile") {
+      const mile = (distance * 0.621371).toFixed(3);
+      result.innerHTML = `${distance} Km(s) = ${mile} Mile(s)`;
+    } else {
+      const km = (distance * 1.609344).toFixed(3);
+      result.innerHTML = `${distance} Mile(s) = ${km} Km(s)`;
+    }
   }
 });
