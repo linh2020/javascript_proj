bfcd6e808770a7e192fec9a9d1f8e12c850ced83
Linh Huynh <cdev.linh@gmail.com>
2024-01-22 22:12:39 -0800

    Created Basic Form with Browser Validation

--- jquery_examples/Game_of_PIG/script.js
+++ jquery_examples/Game_of_PIG/script.js
@@ -24,3 +24,29 @@ let gameData = {
   index: 0,
   gameEnd: 29,
 };
+
+//
+startGame.addEventListener("click", function (e) {
+  gameData.index = Math.round(Math.random());
+  //   console.log(gameData.index); // 0 or 1
+  gameControl.innerHTML = "<h2>The Game Has Started</h2>";
+  gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+
+  document.getElementById("quit").addEventListener("click", function (e) {
+    location.reload(); // used to reload the current page. This can be useful in scenarios where you want to refresh the content of the page, for example, after certain actions have been performed.
+  });
+
+  console.log("set up the turn!");
+});
+
+function setUpTurn(param) {
+  game.innerHTML = `<p>Roll the dice for the ${
+    gameData.players[gameData.index]
+  }</p>`;
+
+  actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+
+  document.getElementById("roll").addEventListener("click", function (e) {
+    console.log("roll the dice");
+  });
+}
