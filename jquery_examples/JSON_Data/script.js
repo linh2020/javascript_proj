471c464f08957dc5c94a7a3d56dcb8b64461362f
Linh Huynh <cdev.linh@gmail.com>
2024-01-28 00:52:48 -0800

    Created Working with JSON Data

---
+++ jquery_examples/JSON_Data/script.js
@@ -0,0 +1,16 @@
+// //
+// document.getElementById("loaddata").addEventListener("click", getData);
+
+// async function getData() {
+//   //
+//   document.getElementById("remotedata").innerHTML =
+//     '<img src="./images/spinner.gif" alt="">';
+
+//   const fetchPromise = await fetch(
+//     "https://cpe-web-assignments.ucdavis.edu/remotedata/index.php"
+//   );
+
+//   const content = await fetchPromise.text();
+//   document.getElementById("remotedata").innerHTML = content;
+//   //
+// }
