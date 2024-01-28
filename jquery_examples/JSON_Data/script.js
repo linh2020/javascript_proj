d5cb491ae22b6d671819c3e338dd45b9f1546021
Linh Huynh <cdev.linh@gmail.com>
2024-01-28 01:16:45 -0800

    Completed Async and Await with JSON

--- jquery_examples/JSON_Data/script.js
+++ jquery_examples/JSON_Data/script.js
@@ -1,10 +1,10 @@
 //
-document.getElementById("loaddata").addEventListener("click", getData);
-
-function getData() {
+// Fetch and Promise with JSON
+document.getElementById("loaddata-1").addEventListener("click", getDataFP);
+function getDataFP() {
   //
-  document.getElementById("remotedata").innerHTML =
-    '<img src="./images/spinner.gif" alt="" />';
+  document.getElementById("remotedata-1").innerHTML =
+    '<img src="./images/spinner.gif" alt="" /><hr>';
 
   const fetchPromise = fetch(
     "https://cpe-web-assignments.ucdavis.edu/remotedata/data.php"
@@ -13,10 +13,27 @@ function getData() {
   fetchPromise.then(function (response) {
     response.json().then(function (data) {
       //
-      document.getElementById("remotedata").innerHTML = outputHTML(data);
+      document.getElementById("remotedata-1").innerHTML = outputHTML(data);
       // console.log(data);
     });
   });
+  //
+}
+
+// Async and Await with JSON
+document.getElementById("loaddata-2").addEventListener("click", getDataAA);
+
+async function getDataAA() {
+  //
+  document.getElementById("remotedata-2").innerHTML =
+    '<img src="./images/spinner.gif" alt="" /><hr>';
+
+  const fetchPromise = await fetch(
+    "https://cpe-web-assignments.ucdavis.edu/remotedata/data.php"
+  );
+
+  const data = await fetchPromise.json();
+  document.getElementById("remotedata-2").innerHTML = outputHTML(data);
   //
 }
 
@@ -25,7 +42,8 @@ function outputHTML(data) {
   const html = `<div>
   <h2>${data.sleep_time} Seconds</h2>
   <p>${data.time_requested}</p>
-  <p>${data.time_processed}</p>  
+  <p>${data.time_processed}</p>
+  <hr>  
   </div>`;
 
   return html;
